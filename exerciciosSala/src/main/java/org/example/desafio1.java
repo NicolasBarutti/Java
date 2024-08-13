65084a08a79849a1b6ea9cd2e94d9fae511d0618
NicolasBarutti <nicolasbarutti2003@gmail.com>
2024-08-13 08:22:21 -0300

    ajuste

--- exerciciosSala/src/main/java/org/example/desafio1.java
+++ exerciciosSala/src/main/java/org/example/desafio1.java
@@ -4,7 +4,7 @@ public class desafio1 {
     public static void main(String[] args) {
 
 
-//        Crie um programa que leia um vetor de 10 inteiros e conte quantos sao pares e quantos sao impares
+//       1 Crie um programa que leia um vetor de 10 inteiros e conte quantos sao pares e quantos sao impares
 
 //        int[]vetor = {1, 4, 6, 8, 9, 12, 14, 17, 20, 21};
 //
@@ -24,7 +24,7 @@ public class desafio1 {
 //        System.out.println("quantidade de numeros pares" + numerosPar);
 //        System.out.println("qauntidade de numeros impares" + numerosImpar);
 
-//        crie um programa que leia um vetor de 5 inteiros e imprima o vetor na ordem inversa
+//        2crie um programa que leia um vetor de 5 inteiros e imprima o vetor na ordem inversa
 
 //        int[] vetor2 = {1, 5, 6, 8, 10};
 //
@@ -33,7 +33,7 @@ public class desafio1 {
 //        }
 
 
-//        crie um progama que leia um vetor de 8 inteiros e determine o maior e o menor valor presente no vetor
+//       3 crie um progama que leia um vetor de 8 inteiros e determine o maior e o menor valor presente no vetor
        int[] vetor3 = {1, 3, 4, 6, 10, 32, 2, 9};
 
        int minimo = vetor3[0] ;
@@ -50,7 +50,7 @@ public class desafio1 {
         System.out.println(minimo);
         System.out.println(maximo);
 
-//    crie um progama que leia um vetor de 6 inteiros e calcule a media desses numeros
+//   4 crie um progama que leia um vetor de 6 inteiros e calcule a media desses numeros
 
 //      int[] vetor4 = {1, 5, 8, 9, 3, 2};
 //
