d1c69935ed29b57dcf7e2039ccfb516c59d4a92b
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-14 10:34:32 -0300

    aula

--- smn2Semestre2/src/main/java/org/example/Main.java
+++ smn2Semestre2/src/main/java/org/example/Main.java
@@ -1,39 +1,95 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        List<Integer>listaInteiros = new ArrayList<>();
 
-        listaInteiros.add(10);
-        listaInteiros.add(30);
-        listaInteiros.add(20);
+        //exercicio
 
-        System.out.println(listaInteiros.size());
+        Pessoa pessoa1 = new Pessoa("Nicolas", 20, "1234567890");
+        Pessoa pessoa2 = new Pessoa("Lucas", 29, "1234543212");
+        Pessoa pessoa3 = new Pessoa("Caio", 11, "12345321232");
 
-        printList(listaInteiros);
+        Pessoa[] arraySimples = {pessoa1, pessoa2, pessoa3};
 
-        listaInteiros.remove(1);
-
-//      //ultimo elemneto
-        System.out.println(listaInteiros.getLast());
-
-        //primeiro elemento
-        listaInteiros.getFirst();
-
-        listaInteiros.add(30);
-        
-
-    }
-
-    private static void printList(List<Integer> listaInteiros) {
-        System.out.println();
-        for (Integer integer : int){
+        for (Pessoa pessoa : arraySimples) {
+            System.out.println(pessoa.getNome() + pessoa.getIdade() + pessoa.getCpf());
 
         }
+
     }
 }
+
+
+//        Set<String> Pessoa = new HashSet<>();
+
+
+
+//        List<Integer>listaInteiros = new ArrayList<>();
+//        // linkedList não compensa quando o for estiver no programa
+//        list<Integer>linkedList = new linkedList<>();
+//
+//        listaInteiros.add(10);
+//        listaInteiros.add(30);
+//        listaInteiros.add(20);
+//
+//        System.out.println(listaInteiros.size());
+//
+//        printList(listaInteiros);
+//
+//        listaInteiros.remove(1);
+//
+////      //ultimo elemneto
+//        System.out.println(listaInteiros.getLast());
+//
+//        //primeiro elemento
+//        System.out.println(listaInteiros.getFirst());
+//
+//        listaInteiros.add(30);
+//        listaInteiros.add(30);
+//        listaInteiros.add(30);
+//        listaInteiros.add(30);
+//        listaInteiros.add(30);
+//
+//        printList(listaInteiros);
+//
+//        System.out.println(listaInteiros.getLast());
+//
+//        listaInteiros.removeLast();
+
+        //        private static void printList (List < Integer > listaInteiros) {
+//            System.out.println();
+//            for (Integer integer : list) {
+//                System.out.println(integer);
+//            }
+//            System.out.println();
+//        }
+
+//        Set<String> nomes = new HashSet<>();
+//        nomes.add("Maria");
+//        nomes.add("João");
+//        nomes.add("Pedro");
+//        nomes.add("Maria");
+//        nomes.add("Carlos");
+//
+//        printList(nomes);
+//
+//
+//        for (String nome : nomes) {
+//            System.out.println(nome);
+//        }
+//}
+//
+//
+//
+//        private static void printList (Set<String> list) {
+//            System.out.println();
+//            for (String nome : list) {
+//                System.out.println(nome);
+//                System.out.println(Objects.hash(nome));
+//            }
+//            System.out.println();
+//        }
